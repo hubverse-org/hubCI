e74b5ebe3aa972ca117bf279347000185ae76967
Anna Krystalli <annakrystalli@googlemail.com>
2025-12-18 17:51:45 +0200

    Migrate to Air code formatting
    - Set up Air config via usethis::use_air()
    - Add format-check.yaml workflow
    - Update CONTRIBUTING.md with Air setup instructions
    - Reformat all R code
    
    Related to hubverse-org/hubDevs#45

--- tests/testthat/test-use_hub_github_action.R
+++ tests/testthat/test-use_hub_github_action.R
@@ -3,25 +3,28 @@ test_that("use_hub_github_action works", {
   orig_wd <- getwd()
   on.exit(setwd(orig_wd))
 
-  usethis::create_project(proj_path,
-    rstudio = FALSE,
-    open = FALSE
-  )
+  usethis::create_project(proj_path, rstudio = FALSE, open = FALSE)
   setwd(proj_path)
   use_hub_github_action(name = "validate-submission")
 
   ga_path <- ".github/workflows/validate-submission.yaml"
-  expect_true(".github" %in% fs::path_file(
-    fs::dir_ls(proj_path, all = TRUE)
-  ))
+  expect_true(
+    ".github" %in%
+      fs::path_file(
+        fs::dir_ls(proj_path, all = TRUE)
+      )
+  )
   expect_true(fs::file_exists(ga_path))
   expect_gt(length(readLines(ga_path)), 0)
   expect_false(any(grepl("remotes::install_github", readLines(ga_path))))
 
   fs::file_delete(ga_path)
-  expect_false(ga_path %in% fs::path_file(
-    fs::dir_ls(proj_path, all = TRUE)
-  ))
+  expect_false(
+    ga_path %in%
+      fs::path_file(
+        fs::dir_ls(proj_path, all = TRUE)
+      )
+  )
 
   use_hub_github_action(name = "validate-submission", ref = "v0.0.1")
 
